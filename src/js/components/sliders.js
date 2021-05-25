76c21bc0b1f6b4ed43789b3f4f60be7f607070f7
Pohurskyi Artem <46791938+PohurskyiArtem@users.noreply.github.com>
2021-05-25 15:38:30 +0300

    test

---
+++ src/js/components/sliders.js
@@ -0,0 +1,72 @@
+import Swiper from '../vendor/swiper.min.js';
+
+const projectsSlider = new Swiper(document.querySelector(".projects__slider"), {
+  loop: true,
+  allowTouchMove: false,
+  spaceBetween: 28,
+  navigation: {
+    nextEl: '.projects-slider-nav__next-btn',
+    prevEl: '.projects-slider-nav__prev-btn'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    }
+  },
+  pagination: {
+    el: '.projects__slider__pag',
+    type: 'bullets',
+    clickable: true
+  }
+})
+
+const cardGallery = new Swiper(document.querySelectorAll(".project-card__gallery"), {
+  loop: true,
+  pagination: {
+    el: ".project-card__swiper-pag",
+    type: 'bullets',
+    clickable: false
+  }
+})
+
+const portfolioSlider = new Swiper(document.querySelector(".portfolio-slider"), {
+  loop: true,
+  pagination: {
+    el: ".portfolio-slider__pag",
+    type: 'bullets',
+    clickable: true
+  }
+})
+
+const previewSlider = new Swiper(document.querySelectorAll(".review-card__preview-slider"), {
+  loop: true,
+  allowTouchMove: false,
+  navigation: {
+    nextEl: '.review-card__preview-slider__next-btn',
+    prevEl: '.review-card__preview-slider__prev-btn'
+  },
+  spaceBetween: 15,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    }
+  },
+})
+
+const reviewSlider = new Swiper(document.querySelector(".reviews__slider"), {
+  loop: true,
+  allowTouchMove: false,
+  navigation: {
+    nextEl: '.reviews__slider__next-btn',
+    prevEl: '.reviews__slider__prev-btn'
+  },
+  pagination: {
+    el: ".reviews__slider-pag",
+    type: 'bullets',
+    clickable: true
+  }
+})
+
+
+
+
