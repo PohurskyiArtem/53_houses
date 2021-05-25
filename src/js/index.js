1a26c115b509b54bace5be658fb3a8744f7a67fb
Pohurskyi Artem <46791938+PohurskyiArtem@users.noreply.github.com>
2021-05-25 22:31:54 +0300

    Delete src directory

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import "./components/sliders";
-
-import {resizeContent} from "./functions/resize";
-import {scrollTo} from "./functions/smooth-scroll";
-import {disableScroll, enableScroll} from "./functions/stop-scroll";
-// import './vendor/focus-visible.min.js';
-
