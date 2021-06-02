10c43619393626bb95873ff215335e43226b6c90
Pohurskyi Artem <46791938+PohurskyiArtem@users.noreply.github.com>
2021-06-02 14:29:21 +0300

    fixes 2

--- src/js/components/header-nav.js
+++ src/js/components/header-nav.js
@@ -16,6 +16,16 @@ document.querySelectorAll(".header-link").forEach(link => {
   })
 })
 
+document.querySelectorAll(".feedback__list__item").forEach(btn => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".burger-btn").classList.remove("active");
+    document.querySelector(".header__bottom").classList.remove("header__bottom--active");
+    document.querySelector(".header__top").classList.remove("header__top--active");
+    document.querySelector("body").classList.remove("menu-open");
+    document.querySelector(".header__mobile-white-bg").classList.remove("header__mobile-white-bg--open");
+  })
+})
+
 document.querySelectorAll(".header-nav__item--dropdown svg").forEach(btn => {
   btn.addEventListener('click', e => {
     e.target.closest(".header-nav__item--dropdown").classList.toggle("header-nav__item--dropdown--open")
--- src/js/components/project-info.js
+++ src/js/components/project-info.js
@@ -10,18 +10,18 @@ navList.forEach(el => {
   })
 })
 
-document.querySelectorAll(".open-plan").forEach(el => {
-  el.addEventListener("click", e => {
-    let set = e.target.getAttribute("data-set");
+  document.querySelectorAll(".open-plan").forEach(el => {
+    el.addEventListener("click", e => {
+      let set = e.target.getAttribute("data-set");
 
-    let item = document.querySelector(`.product-info .product-info-nav__item[data-set="${set}"]`);
-    let itemRect = item.getBoundingClientRect();
-    let itemOffset = document.body.scrollTop + itemRect.top + 600;
-    console.log(itemOffset)
-    window.scrollTo(0, itemOffset);
-    navList.forEach(el => {
-      el.classList.remove("product-info-nav__item--active")
+      let item = document.querySelector(`.product-info .product-info-nav__item[data-set="${set}"]`);
+      // let itemRect = item.getBoundingClientRect();
+      // let itemOffset = document.body.scrollTop + itemRect.top + 600;
+      // console.log(itemOffset)
+      // window.scrollTo(0, itemOffset);
+      navList.forEach(el => {
+        el.classList.remove("product-info-nav__item--active")
+      })
+      item.classList.add("product-info-nav__item--active");
     })
-    item.classList.add("product-info-nav__item--active");
   })
-})
